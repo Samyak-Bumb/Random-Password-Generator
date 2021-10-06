3cc55efc4e59cff60fda72b6537ed49685573a61
Samyak-Bumb <vijaybumb0575@gmail.com>
2021-10-06 19:03:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const alertBox = document.querySelector('.alert-box')    
-function getPassword() {
-const chars = '01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#%^&*()_+~|}{[]:;?><,./-="';
-const passwordLength = 16;
+const alertBox = document.querySelector('.alert-box')
 
-let password = '';
-for (let i = 0; i < passwordLength; i++) {
-    const randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber + 1);
+function getPassword(){
+    const chars = '01234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#%^&*()_+~|}{[]:;?><,./-="';
+    const passwordLength = 16;
+
+    let password = '';
+    for (let i = 0; i < passwordLength; i++){
+        const randomNumber = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    document.querySelector('#password').value = password;
+    // alertBox.innerHTML = 'New Password Copied:<br><br>' + password;
 }
-document.querySelector('#password').value = password;
-// alertBox.innerHTML = 'New Password Copied:<br><br>' + password;
+function copyPassword(){
+    const copyPassText = document.querySelector('#password');
+    copyPassText.select();
+    copyPassText.setSelectionRange(0, 9999);
+    document.execCommand('copy');
+    alertBox.classList.toggle('active')
+    setTimeout(() => alertBox.classList.toggle('active'), 2700);
 }
-function copyPassword() {
-const copyPassText = document.querySelector('#password');
-copyPassText.select();
-copyPassText.setSelectionRange(0, 9999);
-document.execCommand('copy');
-alertBox.classList.toggle('active')
-setTimeout(() => alertBox.classList.toggle('active'), 2700);
-}
+
 document.querySelector('#btn').addEventListener('click', getPassword);
 document.querySelector('.copy').addEventListener('click', copyPassword);
